1a51a2ee3de96eed8f59c525e590f58004e1f99c
z.khaddan <zakaria.khaddan@it-adaptive.com>
2025-04-27 23:16:35 +0100

    first commit

---
+++ src/test/java/com/adaptive/demojenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaptive.demojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
